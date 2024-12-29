1b378001f6feab4d899ede2f1651c19f884efcb1
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-12-29 17:49:17 +0200

    feat(health-url): Add endpoint for url and add healthcheck to workflows

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 
+
+//Health check
+app.get('/health', (req, res) => {
+  res.status(200).send('OK')
+})
+
+
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
